93aee547d868cc26f69a206156335235bbd00f34
Yusuf-progX <imdyusufff@gmail.com>
2024-12-26 22:44:31 +0530

    my second code

---
+++ JavaStdinAndStdout.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class JavaStdinAndStdout{
+    public static void main(String[] agrs){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
